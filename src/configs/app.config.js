38e1d30706e8f45c544b47cf728aa29543844838
lazizbeksultanov <lazizbeksultonov0804@gmail.com>
2022-10-22 14:01:50 +0500

    Initial commit

---
+++ src/configs/app.config.js
@@ -0,0 +1,9 @@
+const appConfig = {
+    apiPrefix: '/api',
+    authenticatedEntryPath: '/home',
+    unAuthenticatedEntryPath: '/sign-in',
+    tourPath: '',
+    enableMock: false
+}
+
+export default appConfig
